e63fe8da29f4da0c3c3f602d13524358c5682936
Fabian Schenkel <fabian.schenkel@gmail.com>
2013-08-04 10:57:21 +0200

    Now using "kdesudo" to enable sudoing under X

--- scripts/toggle-bluetooth.sh
+++ scripts/toggle-bluetooth.sh
@@ -2,5 +2,5 @@
 
 ENABLED=$(grep -c "enabled" /proc/acpi/ibm/bluetooth)
 
-[[ $ENABLED -eq 0 ]] && sudo sh -c 'echo "enable"  > /proc/acpi/ibm/bluetooth'
-[[ $ENABLED -eq 1 ]] && sudo sh -c 'echo "disable" > /proc/acpi/ibm/bluetooth'
+[[ $ENABLED -eq 0 ]] && kdesudo sh -c 'echo "enable"  > /proc/acpi/ibm/bluetooth'
+[[ $ENABLED -eq 1 ]] && kdesudo sh -c 'echo "disable" > /proc/acpi/ibm/bluetooth'
